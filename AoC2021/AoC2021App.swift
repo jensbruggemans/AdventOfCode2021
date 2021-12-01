24484c5457cc483db8b196ad00ffa123e6ef3a96
Jens Bruggemans <jens.bruggemans@persgroep.be>
2021-12-01 20:55:32 +0100

    Initial Commit

---
+++ AoC2021/AoC2021App.swift
@@ -0,0 +1,17 @@
+//
+//  AoC2021App.swift
+//  AoC2021
+//
+//  Created by Jens Bruggemans on 01/12/2021.
+//
+
+import SwiftUI
+
+@main
+struct AoC2021App: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
