7271f9cc22cdf3c4b2c683fefc6eb48b559e4648
Jens Bruggemans <jens@iscape.be>
2021-12-04 22:59:48 +0100

    Day 4

---
+++ AoC2021/Day4/Day4.swift
@@ -0,0 +1,108 @@
+//
+//  Day4.swift
+//  AoC2021
+//
+//  Created by Jens Bruggemans on 04/12/2021.
+//
+
+import Foundation
+import SwiftUI
+
+struct Day4: Problem {
+    let title: String = "Day 4"
+    
+    var solutionView: AnyView {
+        let inputLines = InputHelper.stringArray(forDay: 4)
+        let numbersToDraw = inputLines[0].components(separatedBy: ",").map{ Int($0)! }
+        let boardsInputLines = inputLines[1..<inputLines.count]
+        var boardNumberArrays = [[[Int]]]()
+        for line in boardsInputLines {
+            if line.isEmpty {
+                boardNumberArrays.append([[Int]]())
+            } else {
+                boardNumberArrays[boardNumberArrays.count-1].append(line.components(separatedBy: .whitespaces).compactMap{ Int($0)})
+            }
+        }
+        let boards = boardNumberArrays.map{ BingoBoard(numbers: $0) }
+        return AnyView(Day4SolutionView(boards: boards, numbersToDraw: numbersToDraw))
+    }
+}
+
+class BingoBoard {
+    struct Position: Hashable {
+        let row: Int
+        let column: Int
+    }
+    
+    let numbers: [[Int]]
+    var markedPositions = Set<Position>()
+    var score: Int?
+    var order: Int?
+    
+    var amountOfRows: Int {
+        return numbers.count
+    }
+    
+    var amountOfColumns: Int {
+        return numbers.first?.count ?? 0
+    }
+    
+    init(numbers: [[Int]]) {
+        self.numbers = numbers
+    }
+    
+    func markNumber(_ number: Int) {
+        for (row, rowNumbers) in numbers.enumerated() {
+            if let column = rowNumbers.firstIndex(of: number) {
+                markedPositions.insert(Position(row: row, column: column))
+            }
+        }
+    }
+    
+    func rowIsMarked(_ row: Int) -> Bool {
+        return markedPositions.filter{ $0.row == row }.count == amountOfColumns
+    }
+    
+    func columnIsMarked(_ column: Int) -> Bool {
+        return markedPositions.filter{ $0.column == column }.count == amountOfRows
+    }
+    
+    var amountOfMarkedRows: Int {
+        return Array(0..<amountOfRows).reduce(0){ $0 + (rowIsMarked($1) ? 1 : 0) }
+    }
+    
+    var amountOfMarkedColumns: Int {
+        return Array(0..<amountOfColumns).reduce(0){ $0 + (columnIsMarked($1) ? 1 : 0) }
+    }
+    
+    var amountOfMarkedRowsOrColumns: Int {
+        return amountOfMarkedRows + amountOfMarkedColumns
+    }
+    
+    var markedNumbers: Set<Int> {
+        var markedNumbers = Set<Int>()
+        for row in 0..<amountOfRows {
+            for column in 0..<amountOfColumns {
+                let position = Position(row: row, column: column)
+                if markedPositions.contains(position) {
+                    markedNumbers.insert(numbers[row][column])
+                }
+            }
+        }
+        return markedNumbers
+    }
+    
+    var unmarkedNumbers: Set<Int> {
+        return allNumbers.subtracting(markedNumbers)
+    }
+    
+    var allNumbers: Set<Int> {
+        var allNumbers = Set<Int>()
+        for row in 0..<amountOfRows {
+            for column in 0..<amountOfColumns {
+                allNumbers.insert(numbers[row][column])
+            }
+        }
+        return allNumbers
+    }
+}
---
+++ AoC2021/Day4/Day4SolutionView.swift
@@ -0,0 +1,94 @@
+//
+//  Day4SolutionView.swift
+//  AoC2021
+//
+//  Created by Jens Bruggemans on 04/12/2021.
+//
+
+import SwiftUI
+
+struct Day4SolutionView: View {
+    let boards: [BingoBoard]
+    @State var incompleteBoards: [BingoBoard]
+    
+    var gridItemLayout = [GridItem].init(repeating: GridItem(.flexible(minimum: 0)), count: 2)
+    
+    @State var markedNumbers = Set<Int>()
+    @State var numbersToDraw: [Int]
+    
+    init(boards: [BingoBoard], numbersToDraw: [Int]) {
+        self.boards = boards
+        incompleteBoards = boards
+        self.numbersToDraw = numbersToDraw
+    }
+    
+    var body: some View {
+        ScrollView {
+            VStack {
+                NumbersView(markedNumbers: markedNumbers).padding(EdgeInsets(top: 0, leading: 0, bottom: 15, trailing: 0))
+                Text("amount completed: \(boards.count - incompleteBoards.count)").font(.system(size: 15, weight: .bold, design: .monospaced)).foregroundColor(Color(hex: "fefffe"))
+            }.padding(30)
+            LazyVGrid(columns: gridItemLayout, spacing: 20) {
+                ForEach((0..<100), id: \.self) { i in
+                    let board = boards[i]
+                    VStack {
+                        BingoCardView(numbers: board.numbers, markedNumbers: board.score == nil ? markedNumbers : board.markedNumbers).frame(width: 150, height: 150, alignment: .center)
+                        let score = board.score != nil ? "\(board.score!)" : "___"
+                        let order = board.order != nil ? "\(board.order!)" : "___"
+                        Text("score: \(score)\norder: \(order)").font(.system(size: 15, weight: .bold, design: .monospaced)).foregroundColor(Color(hex: "fefffe"))
+                    }.padding().background(Color(hex: "0b3954")).clipShape(RoundedRectangle(cornerSize: CGSize(width: 5, height: 5)))
+                }
+            }
+        }.toolbar {
+            Button("Draw number") {
+                guard let number = numbersToDraw.first else {
+                    return
+                }
+                for board in incompleteBoards {
+                    board.markNumber(number)
+                    if board.amountOfMarkedRowsOrColumns != 0 {
+                        board.score = number * board.unmarkedNumbers.reduce(0, +)
+                        board.order = boards.count - incompleteBoards.count + 1
+                    }
+                }
+                incompleteBoards = boards.filter{ $0.amountOfMarkedRowsOrColumns == 0 }
+                markedNumbers.insert(number)
+                numbersToDraw = Array(numbersToDraw.dropFirst())
+            }
+        }
+    }
+}
+
+struct BingoCardView: View {
+    let numbers: [[Int]]
+    let markedNumbers: Set<Int>
+    private let gridItemLayout = [GridItem].init(repeating: GridItem(.flexible()), count: 5)
+    var body: some View {
+        LazyVGrid(columns: gridItemLayout) {
+            ForEach((0..<25), id: \.self) { i in
+                ZStack {
+                    let number = numbers[i/5][i%5]
+                    let color: Color = markedNumbers.contains(number) ? Color(hex: "e0ff4f") : Color(hex: "bfd7ea")
+                    Circle().fill(color).frame(width: 23, height: 23, alignment: .center)
+                    Text("\(number)").font(.system(size: 12, weight: .bold, design: .monospaced)).foregroundColor(.black)
+                }
+            }
+        }
+    }
+}
+
+struct NumbersView: View {
+    let markedNumbers: Set<Int>
+    private let gridItemLayout = [GridItem].init(repeating: GridItem(.flexible()), count: 10)
+    var body: some View {
+        LazyVGrid(columns: gridItemLayout) {
+            ForEach((0...99), id: \.self) { number in
+                ZStack {
+                    let color: Color = markedNumbers.contains(number) ? Color(hex: "e0ff4f") : Color(hex: "bfd7ea")
+                    Circle().fill(color).frame(width: 23, height: 23, alignment: .center)
+                    Text("\(number)").font(.system(size: 12, weight: .bold, design: .monospaced)).foregroundColor(.black)
+                }
+            }
+        }
+    }
+}
