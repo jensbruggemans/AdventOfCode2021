1b304412c618af93c797866b72b7432c0ef5260f
Jens Bruggemans <jens@iscape.be>
2021-12-12 00:01:10 +0100

    Day 8 cleanup

--- AoC2021/Day8/Day8.swift
+++ AoC2021/Day8/Day8.swift
@@ -20,8 +20,7 @@ struct Day8: Solution {
         let characterSetsArrays: [[Set<Character>]] = stringArrays.map { $0.map { Set($0) }}
         displayPuzzles = characterSetsArrays.map{ DisplayPuzzle(signals: Array($0[0...9]), output: Array($0[10...13])) }
         part1 = displayPuzzles.reduce(0) { $0 + $1.output.filter{ [2,3,4,7].contains($0.count) }.count }
-        var index = 0
-        part2 = displayPuzzles.reduce(0) { index += 1 ; print(index); return $0 + $1.solve() }
+        part2 = displayPuzzles.reduce(0) { return $0 + $1.solve() }
     }
     
     var view: AnyView {
@@ -34,24 +33,24 @@ struct DisplayPuzzle {
     var output: [Set<Character>]
     
     func solve() -> Int {
-        let allSignslsInOneString = signals.reduce("") { $0 + Array(arrayLiteral: $1).joined() }
-        let amountOfCharacter: [Character: Int] = allSignslsInOneString.reduce([Character: Int]()) {
+        let allSignalsInOneString = signals.reduce("") { $0 + Array(arrayLiteral: $1).joined() }
+        let amountOfCharacter: [Character: Int] = allSignalsInOneString.reduce([Character: Int]()) {
             var result = $0
             result[$1] = (result[$1] ?? 0) + 1
             return result
         }
-        let allCharacters = "abcdefg"
         let one = signals.first { $0.count == 2 }!
         let four = signals.first { $0.count == 4 }!
         let seven = signals.first { $0.count == 3 }!
         let eight = signals.first { $0.count == 7 }!
+        let allCharacters = "abcdefg"
         let aMapping = allCharacters.first { amountOfCharacter[$0] == 8 && !one.contains($0) }!
         let bMapping = allCharacters.first { amountOfCharacter[$0] == 6 }!
         let cMapping = allCharacters.first { amountOfCharacter[$0] == 8 && one.contains($0) }!
-        let dMapping = allCharacters.first { amountOfCharacter[$0] == 7 && four.contains($0)}!
+        let dMapping = allCharacters.first { amountOfCharacter[$0] == 7 && four.contains($0) }!
         let eMapping = allCharacters.first { amountOfCharacter[$0] == 4 }!
         let fMapping = allCharacters.first { amountOfCharacter[$0] == 9 }!
-        let gMapping = allCharacters.first { amountOfCharacter[$0] == 7 && !four.contains($0)}!
+        let gMapping = allCharacters.first { amountOfCharacter[$0] == 7 && !four.contains($0) }!
         let two: Set = [aMapping, cMapping, dMapping, eMapping, gMapping]
         let three: Set = [aMapping, cMapping, dMapping, fMapping, gMapping]
         let five: Set = [aMapping, bMapping, dMapping, fMapping, gMapping]
